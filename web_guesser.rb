9beb837c35ac712865e9011f83b2052522835679
Alex Tideman <alex.tideman@gmail.com>
2015-06-29 17:57:38 -0600

    Adding text color

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,29 +1,39 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-  SECRET_NUMBER = rand(100)
+    SECRET_NUMBER = rand(100)
 
   def check_guess(guess)
     number = SECRET_NUMBER
-    if guess.to_i == number
-      ["You got it right!" + "\n" + "The SECRET NUMBER is #{number}", "green"]
-    elsif (guess.to_i - number) > 5
-      ["Way too high!", "red"]
-    elsif guess.to_i > number
-      ["Too high!", "indianred"]
-    elsif (number - guess.to_i) > 5
-      ["Way too low", "red"]
-    elsif guess.to_i < number
-      ["Too low", "indianred"]
-    end
+    
+      if guess.to_i == number
+        ["You got it right!","green","yellow","The SECRET NUMBER is #{number}"]
+      elsif (guess.to_i - number) > 5
+        ["Way too high!", "red", "black"]
+      elsif guess.to_i > number
+        ["Too high!", "indianred", "black"]
+      elsif (number - guess.to_i) > 5
+        ["Way too low", "red", "black"]
+      elsif guess.to_i < number
+        ["Too low", "indianred", "black"]
+      end
   end
 
-get '/' do
-  guess = params['guess']
-  message = check_guess(guess).first
-  color = check_guess(guess).last
-  erb :index, :locals => {:message => message,
-                          :color => color}
-end
+
+
+  get '/' do
+    guess = params['guess']
+    message = check_guess(guess)[0]
+    color = check_guess(guess)[1]
+    text_color = check_guess(guess)[2]
+    answer = check_guess(guess)[3]
+
+    erb :index, :locals => {:message => message,
+                            :color => color,
+                            :text_color => text_color,
+                            :answer => answer}
+  end
+
+
 
 
